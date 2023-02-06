c7890e4322bdfb06f8d4f8e1a36d623df8e25eea
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2023-02-06 11:28:13 +0200

    finished 2th

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Search = ({ value, onChange, children }) => (
+  <div>
+    <label htmlFor="search">{children}</label>
+    <input id="search" type="text" value={value} onChange={onChange} />
+  </div>
+);
+
+export const App = () => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = ({ target: { value } }) => setSearch(() => value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search value={search} onChange={handleChange}>
+        Search
+      </Search>
+      <p>Searches for {search ? search : ""}</p>
     </div>
   );
-}
-
-export default App;
+};
