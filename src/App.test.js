02a4852b08590a527a52caaa55f2f1b82084c534
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2023-02-08 14:23:16 +0200

    finished less-5

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,44 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { App } from "./App";
-it("renders learn react link", async () => {
-  render(<App />);
-  // expect(screen.queryByText(/Searches for React/i)).toBeNull();
-  expect(screen.queryByText(/Loged in as/i)).toBeNull();
-  expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument();
-  expect(screen.getByLabelText(/search/i)).not.toBeRequired();
-  expect(screen.getByLabelText(/search/i)).toBeEmpty();
-  expect(screen.getByLabelText(/search/i)).toHaveAttribute("id");
 
+describe("App", () => {
+  it("renders App component", async () => {
+    render(<App />);
+    await screen.findByText(/Logged in as/i);
+    expect(screen.queryByText(/Searches for React/i)).toBeNull();
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "React" },
+    });
+    expect(screen.getByText(/Searches for react/i)).toBeInTheDocument();
+  });
+});
+
+describe("events", () => {
+  it("checkbox click", () => {
+    const handleChange = jest.fn();
+    const { container } = render(
+      <input type="checkbox" onChange={handleChange} />
+    );
+    const checkbox = container.firstChild;
+
+    expect(container).not.toBeChecked();
+    fireEvent.click(checkbox);
+    fireEvent.click(checkbox);
+    expect(handleChange).toHaveBeenCalledTimes(2);
+    fireEvent.click(checkbox);
+
+    // expect(checkbox).toBeChecked();
+    expect(container).toMatchSnapshot();
+  });
+
+  it("input focus", () => {
+    const { getByTestId } = render(
+      <input type="text" data-testid="simple-input" />
+    );
+    const input = screen.getByTestId("simple-input");
+
+    expect(input).not.toHaveFocus();
+    input.focus();
+    expect(input).toHaveFocus();
+  });
 });
